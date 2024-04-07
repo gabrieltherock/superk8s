b02b6481b838c71642dbb8eca448929c94f1f6be
gabrieltherock <gabrieldarocha2013@hotmail.com>
2024-04-07 10:08:51 -0300

    Initial commit

---
+++ src/main/java/br/com/gabriel/superk8s/Superk8sApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.superk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Superk8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Superk8sApplication.class, args);
+	}
+
+}
